c8cb114c73749b7729ac7ed975d63614431b9310
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-04-10 00:07:33 +0900

    update: app base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
+import adobeLoader from '../utils/adobeloader'
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  
+  useEffect(() => {
+    adobeLoader(document);
+  },[])
+  return (
+    <Layout route={router.pathname}>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
