0a3f36ac7ffd04aa45902daf0912074373ecb81a
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-04-17 00:27:55 +0900

    update: slash

--- src/utils/get_article.ts
+++ src/utils/get_article.ts
@@ -8,6 +8,7 @@ export type PageProps = {
 const getArticle = async(path : string) => {
   const doc = await firestore.collection("blog").doc(path).get();
   const article : PageProps = {title: doc.get("title"), article: doc.get("article")}
+  article.article = article.article.replaceAll("\\n", "\n")
   return article;
 }
 
